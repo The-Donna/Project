e1905d015dc7fdb5737e86439d41a706f0c5f044
aet78 <aet78@tux2.cci.drexel.edu>
2025-04-14 23:18:52 -0400

    login page added

---
+++ db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://alyntetteh:Tetteh_1234@cluster0.8uiqb0o.mongodb.net/Login-b%26c?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB error:', err.message));
